cf39eac62dd519cc01dbe3804fd2448730627422
Pragathi1256 <2420030672@klh.edu.in>
2026-04-03 00:06:14 +0530

    Update base path in Vite configuration

--- Cloud test/vite.config.ts
+++ Cloud test/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/CIS-Hackathon/",   // IMPORTANT: your GitHub repository name
+  base: "CIS-Hackathon",   // IMPORTANT: your GitHub repository name
 
   server: {
     host: true,
